aa1ebb8636b1314058c8f4441573bb75c166396f
BJAJ05 <abayraa227@gmail.com>
2026-09-06 23:45:59 +0800

    add more tests

--- my-first-test/tests/mytest.spec.ts
+++ my-first-test/tests/mytest.spec.ts
@@ -6,4 +6,28 @@ test('амжилттай нэвтрэх', async ({ page }) => {
     await page.getByPlaceholder('Password').fill('secret_sauce');
     await page.getByRole('button', { name: 'Login' }).click();
     await expect(page.getByText('Products')).toBeVisible();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+});
+
+test('амжилтгүй нэвтрэх', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com');
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('wrong_pass');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByText('Products')).not.toBeVisible();
+    await expect(page).not.toHaveURL('https://www.saucedemo.com/inventory.html');
+});
+
+test('бараа сагслах', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com');
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByText('Products')).toBeVisible();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+    await page.getByRole('button', { name: 'Add to cart' }).first().click();
+    await expect(page.getByRole('button', { name: 'Remove' })).toBeDefined();
 });
