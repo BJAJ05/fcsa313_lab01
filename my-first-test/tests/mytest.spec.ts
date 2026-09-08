7ea52a836b33554b79de84c4f5d3a37ddfb065c6
BJAJ05 <abayraa227@gmail.com>
2026-09-08 22:59:02 +0800

    тестүүдэд logout нэмсэн, тайлбар оруулсан

--- my-first-test/tests/mytest.spec.ts
+++ my-first-test/tests/mytest.spec.ts
@@ -2,32 +2,51 @@ import { test, expect } from '@playwright/test';
 
 test('амжилттай нэвтрэх', async ({ page }) => {
     await page.goto('https://www.saucedemo.com');
+    // Login
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_sauce');
     await page.getByRole('button', { name: 'Login' }).click();
+
+    // Нүүр хуудас дээр очсон, текст нь харагдаж байгаа эсэхийг шалгах
     await expect(page.getByText('Products')).toBeVisible();
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+    // Logout
+    await page.getByRole('button', { name: 'Open Menu' }).click();
+    await page.getByRole('link', { name: 'Logout' }).click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/');
 });
 
 test('амжилтгүй нэвтрэх', async ({ page }) => {
     await page.goto('https://www.saucedemo.com');
+    // Login
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('wrong_pass');
     await page.getByRole('button', { name: 'Login' }).click();
 
+    // Нүүр хуудас дээр очоогүй байгаа эсэхийг шалгах
     await expect(page.getByText('Products')).not.toBeVisible();
-    await expect(page).not.toHaveURL('https://www.saucedemo.com/inventory.html');
+    await expect(page).toHaveURL('https://www.saucedemo.com/');
 });
 
 test('бараа сагслах', async ({ page }) => {
     await page.goto('https://www.saucedemo.com');
+    // Login
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_sauce');
     await page.getByRole('button', { name: 'Login' }).click();
 
+    // Нүүр хуудас дээр очсон, текст нь харагдаж байгаа эсэхийг шалгах
     await expect(page.getByText('Products')).toBeVisible();
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
+    // Эхний бүтээгдэхүүний "Add to cart" товчийг дарах
     await page.getByRole('button', { name: 'Add to cart' }).first().click();
-    await expect(page.getByRole('button', { name: 'Remove' })).toBeDefined();
+    // "Remove" товч харагдаж байгаа эсэхийг шалгах
+    await expect(page.getByRole('button', { name: 'Remove' }).first()).toBeVisible();
+
+    // Logout
+    await page.getByRole('button', { name: 'Open Menu' }).click();
+    await page.getByRole('link', { name: 'Logout' }).click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/');
 });
