696e7d46d8444736898d47331269aad94c81dc22
BJAJ05 <abayraa227@gmail.com>
2026-09-09 22:27:03 +0800

    README сайжруулалт, сөрөг тестийн үр дүн хавсаргасан

--- my-first-test/playwright.config.ts
+++ my-first-test/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1, // trace нь унасан тестийг дахин ажиллуулахад ажиллах тул дор хаяж 1 байхаар тохируулсан
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -31,7 +31,7 @@ export default defineConfig({
     testIdAttribute: 'data-test',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
     video: 'on',
   },
 
